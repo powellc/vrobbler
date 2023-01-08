71a8a19491b35fd8babefa6bce987460170d8883
Colin Powell <colin@unbl.ink>
2023-01-08 16:49:13 -0500

    Add context processors for base template

--- vrobbler/settings.py
+++ vrobbler/settings.py
@@ -119,6 +119,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "videos.context_processors.video_lists",
+                "music.context_processors.music_lists",
             ],
         },
     },
