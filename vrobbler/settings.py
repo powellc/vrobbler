1476fe37ca03dcbad461eb758f3e6b6f0e5110c6
Colin Powell <colin@unbl.ink>
2023-01-08 02:51:35 -0500

    Auth is jamming up the works right now

--- vrobbler/settings.py
+++ vrobbler/settings.py
@@ -159,8 +159,8 @@ AUTHENTICATION_BACKENDS = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
