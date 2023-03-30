6c060f24ec2e362d8f8a3ea62014d8fafc37febd
Colin Powell <colin@unbl.ink>
2023-03-30 00:34:17 -0400

    Default timezone to one that respects DST

--- vrobbler/settings.py
+++ vrobbler/settings.py
@@ -235,7 +235,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = os.getenv("VROBBLER_TIME_ZONE", "EST")
+TIME_ZONE = os.getenv("VROBBLER_TIME_ZONE", "America/New_York")
 
 USE_I18N = True
 
