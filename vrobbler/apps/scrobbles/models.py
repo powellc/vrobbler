dbbb2b43a8f01e352d9099c6884bb93c062dd9bf
Colin Powell <colin@unbl.ink>
2023-05-24 19:31:16 -0400

    Add scrobble list to import detail view

--- vrobbler/apps/scrobbles/models.py
+++ vrobbler/apps/scrobbles/models.py
@@ -99,6 +99,14 @@ class BaseFileImportMixin(TimeStampedModel):
             ]
         )
 
+    def scrobbles(self) -> models.QuerySet:
+        scrobble_ids = []
+        for line in self.process_log.split("\n"):
+            sid = line.split("\t")[0]
+            if sid:
+                scrobble_ids.append(sid)
+        return Scrobble.objects.filter(id__in=scrobble_ids)
+
     def mark_started(self):
         self.processing_started = timezone.now()
         self.save(update_fields=["processing_started"])
