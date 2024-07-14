dcd7196010944ddeb7688339cd15d74d32959ba2
Colin Powell <colin@unbl.ink>
2024-07-14 01:15:27 -0400

    Fix drone ntfy version

--- vrobbler/apps/scrobbles/dataclasses.py
+++ vrobbler/apps/scrobbles/dataclasses.py
@@ -29,6 +29,16 @@ class JSONMetadata(JSONWizard):
     def json(self):
         return json.dumps(self.asdict)
 
+    @classmethod
+    def from_dict(cls, env):
+        return cls(
+            **{
+                k: v
+                for k, v in env.items()
+                if k in inspect.signature(cls).parameters
+            }
+        )
+
 
 @dataclass
 class BoardGameScore(JSONMetadata):
